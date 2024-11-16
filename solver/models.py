01974399c882c80617aba06842524adaf3a61b5f
ra2dd <ra2dd@example.com>
2024-11-16 16:12:11 +0100

    add Knight class, add tests

--- solver/models.py
+++ solver/models.py
@@ -6,29 +6,55 @@ rows = [1, 2, 3, 4, 5, 6, 7, 8]
 
 class Figure(ABC):
     def __init__(self, current_field: str):
-        self.current_field = current_field
+        self.current_field: str = current_field
+        self.col: str = self.current_field[0]
+        self.row: int = int(self.current_field[1])
 
     @abstractmethod
-    def list_available_moves(self) -> list:
+    def list_available_moves(self) -> list | set:
         pass
 
-    @abstractmethod
     def validate_move(self, dest_field: str) -> bool:
-        pass
-
-
-class Pawn(Figure):
-    def list_available_moves(self) -> list:
-        col: str = self.current_field[0]
-        row: int = int(self.current_field[1])
-        if row == 8:
-            return []
-        else:
-            return [f"{col}{row+1}"]
-
-    def validate_move(self, dest_field: str) -> bool:
-        avaliable_moves = self.list_available_moves()
+        avaliable_moves: list | set = self.list_available_moves()
         if dest_field in avaliable_moves:
             return True
         else:
             return False
+
+
+class Pawn(Figure):
+    def list_available_moves(self) -> list:
+        if self.row == 8:
+            return []
+        else:
+            return [f"{self.col}{self.row+1}"]
+
+
+class Knight(Figure):
+    def list_available_moves(self) -> set:
+        moves = set()
+        jump_col = [2, -2, 0, 0]
+        jump_row = [0, 0, 2, -2]
+        jump_dir = [1, -1]
+
+        for turn in range(4):
+            for direction in range(2):
+                # Make a two tile jump in certain direction
+                col_index = cols.index(self.col) + jump_col[turn]
+                row_index = rows.index(self.row) + jump_row[turn]
+
+                # Make a one tile jump left or right
+                if jump_col[turn] == 0:
+                    col_index += jump_dir[direction]
+                else:
+                    row_index += jump_dir[direction]
+
+                # Check if after a move a figure is out of board
+                if col_index >= len(cols) or col_index < 0:
+                    continue
+                if row_index >= len(rows) or row_index < 0:
+                    continue
+
+                # Append move to set
+                moves.add(f"{cols[col_index]}{rows[row_index]}")
+        return moves
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,11 @@
-from solver.models import Pawn
+from solver.models import Pawn, Knight
 
 
 def test_initialization():
     pawn = Pawn("A2")
     assert pawn.current_field == "A2", "Pawn should be initialized at A2"
+    assert pawn.col == "A"
+    assert pawn.row == 2
 
 
 def test_pawn_list_available_moves():
@@ -18,3 +20,23 @@ def test_pawn_validate_move():
     pawn = Pawn("A2")
     assert pawn.validate_move("A3") is True
     assert pawn.validate_move("B5") is False
+
+
+def test_knight_list_available_moves():
+    knight = Knight("D4")
+    expected_moves = {"C6", "E6", "F3", "F5", "C2", "E2", "B3", "B5"}
+    assert knight.list_available_moves() == expected_moves
+
+    knight = Knight("A8")
+    expected_moves = {"B6", "C7"}
+    assert knight.list_available_moves() == expected_moves
+
+    knight = Knight("H1")
+    expected_moves = {"F2", "G3"}
+    assert knight.list_available_moves() == expected_moves
+
+
+def test_knight_validate_move():
+    knight = Knight("D4")
+    assert knight.validate_move("C6") is True
+    assert knight.validate_move("D6") is False
