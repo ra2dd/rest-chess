4176a13ac61f1dbc8c8b2289e9f8fa90671fd3d6
ra2dd <radoslawpryw@gmail.com>
2024-12-04 12:10:24 +0100

    combine _list_straight_moves and _list_close_moves

--- solver/models.py
+++ solver/models.py
@@ -32,24 +32,21 @@ class Figure(ABC):
         else:
             return False
 
-    def __indexes_in_board(self, col_index: int, row_index: int) -> bool:
-        if col_index > self.__max_index or col_index < self.__min_index:
-            return False
-        elif row_index > self.__max_index or row_index < self.__min_index:
-            return False
-        else:
-            return True
-
-    def _list_straight_moves(self, jump_col: list, jump_row: list) -> set:
+    def _list_moves(self, jump_col: list, jump_row: list, linear: bool = False) -> set:
         moves = set()
 
         self.__check_array_lengths_equal(len(jump_col), len(jump_row))
+
+        if linear:
+            moves_in_line = self._max_moves
+        else:
+            moves_in_line = 1
 
         for turn in range(len(jump_col)):
             col_index: int = self.__cols.index(self.col)
             row_index: int = self.__rows.index(self.row)
 
-            for _ in range(self._max_moves):
+            for _ in range(moves_in_line):
                 # Move a step
                 col_index += jump_col[turn]
                 row_index += jump_row[turn]
@@ -61,25 +58,13 @@ class Figure(ABC):
                 moves.add(f"{self.__cols[col_index]}{self.__rows[row_index]}")
         return moves
 
-    def _list_close_moves(self, jump_col: list, jump_row: list) -> set:
-        moves = set()
-
-        self.__check_array_lengths_equal(len(jump_col), len(jump_row))
-
-        for turn in range(len(jump_col)):
-            col_index: int = self.__cols.index(self.col)
-            row_index: int = self.__rows.index(self.row)
-
-            # Move a step
-            col_index += jump_col[turn]
-            row_index += jump_row[turn]
-
-            if not self.__indexes_in_board(col_index, row_index):
-                continue
-
-            # Append move to set
-            moves.add(f"{self.__cols[col_index]}{self.__rows[row_index]}")
-        return moves
+    def __indexes_in_board(self, col_index: int, row_index: int) -> bool:
+        if col_index > self.__max_index or col_index < self.__min_index:
+            return False
+        elif row_index > self.__max_index or row_index < self.__min_index:
+            return False
+        else:
+            return True
 
     def __check_array_lengths_equal(self, col_len, row_len):
         if col_len != row_len:
@@ -91,7 +76,6 @@ class Figure(ABC):
 
 class Pawn(Figure):
     def list_available_moves(self) -> set:
-        print(self._max_moves)
         if self.row == self._max_moves:
             return set()
         else:
@@ -102,32 +86,32 @@ class Knight(Figure):
     def list_available_moves(self) -> set:
         jump_col = [2, -2, 2, -2, 1, 1, -1, -1]
         jump_row = [1, 1, -1, -1, 2, -2, 2, -2]
-        return self._list_close_moves(jump_col, jump_row)
+        return self._list_moves(jump_col, jump_row)
 
 
 class Bishop(Figure):
     def list_available_moves(self) -> set:
         jump_col = [1, 1, -1, -1]
         jump_row = [1, -1, 1, -1]
-        return self._list_straight_moves(jump_col, jump_row)
+        return self._list_moves(jump_col, jump_row, linear=True)
 
 
 class Rook(Figure):
     def list_available_moves(self) -> set:
         jump_col = [1, -1, 0, 0]
         jump_row = [0, 0, 1, -1]
-        return self._list_straight_moves(jump_col, jump_row)
+        return self._list_moves(jump_col, jump_row, linear=True)
 
 
 class Queen(Figure):
     def list_available_moves(self) -> set:
         jump_col = [1, 1, -1, -1, 1, -1, 0, 0]
         jump_row = [1, -1, 1, -1, 0, 0, 1, -1]
-        return self._list_straight_moves(jump_col, jump_row)
+        return self._list_moves(jump_col, jump_row, linear=True)
 
 
 class King(Figure):
     def list_available_moves(self) -> set:
         jump_col = [1, 1, -1, -1, 1, -1, 0, 0]
         jump_row = [1, -1, 1, -1, 0, 0, 1, -1]
-        return self._list_close_moves(jump_col, jump_row)
+        return self._list_moves(jump_col, jump_row)
